8430b514760b7ddb8c94df222ee456b2e8230db3
root <yiyi.wang@runtimeverification.com>
2018-08-31 15:53:28 +0000

    Initial commit

---
+++ rv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x 
+
+json_out=`pwd`/errors.json
+report_out=`pwd`/report
+
+apt install -y automake libtool make python zlib1g-dev
+
+autoreconf -f -i
+./configure --disable-silent-rules CC=kcc LD=kcc CFLAGS="-fissue-report=$json_out"
+make -j`nproc`
+make -C tests check
+
+touch $json_out && rv-html-report $json_out -o $report_out
+# rv-upload-report $report_out
